1913c699a91734ed0faedcfbab41bfc8b0e0417b
JosephTong <joseph.tong@titansworld.co>
2023-04-27 16:56:53 +0800

    Drag to aim system

--- Assets/BaseDefense/Script/GunController.cs
+++ Assets/BaseDefense/Script/GunController.cs
@@ -1,67 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine;
+using ExtendedButtons;
 
 public class GunController : MonoBehaviour
 {
-    [SerializeField] private Transform m_Crosshair; // sway camera 
-    [SerializeField][Range(0.1f, 1.5f)] private float m_MouseSensitivity = 0.5f;
+    [Header("Drag to aim")]
+    [SerializeField][Range(0.1f,2)] private float m_AimSensitivity = 0.5f;
+    [SerializeField] private Button2D m_AimBtn;
+    [SerializeField] private Transform m_CrossHair;
+    private Vector2 m_AimDragStartPos = Vector2.zero;
+    private Vector2 m_AimDragEndPos = Vector2.zero;
+    private Vector3 m_CrossHairDragStartPos;
 
-    [SerializeField] private Vector3 m_WeaponToMainCameraOffset = new Vector3(5.5f, -2.5f, 0);
-
-    [SerializeField] private Vector2Int m_TopRight;
-    [SerializeField] private Vector2Int m_BottomLeft;
-    [SerializeField] private PolygonCollider2D m_BaseCameraBorder ;
 
 
-    private void Start()
-    {
-        UpdateBaseCameraBorder();
+    private void Start() {
+        m_AimBtn.onDown.AddListener(()=>{
+            m_AimDragStartPos = Input.mousePosition;
+            m_CrossHairDragStartPos = m_CrossHair.position;
+        });
+        m_AimBtn.onUp.AddListener(()=>{
+            m_AimDragStartPos = Vector2.zero;
+        });
     }
 
-    private void Update()
-    {
-        if (Input.GetAxis("Mouse X") < 1 && Input.GetAxis("Mouse Y") < 1)
-        {
-            m_Crosshair.position += new Vector3(
-                Input.GetAxis("Mouse X"),
-                Input.GetAxis("Mouse Y"),
-                0) * m_MouseSensitivity;
 
-            // prevent crosshair going out of bound
-            if(m_Crosshair.position.x < Mathf.Min(m_TopRight.x,m_BottomLeft.x) ){
-                // Left out of bound
-                m_Crosshair.position = new Vector3(Mathf.Min(m_TopRight.x,m_BottomLeft.x),m_Crosshair.position.y,0);
-            }
+    private void Update() {
+        if(m_AimDragStartPos != Vector2.zero){
+            m_AimDragEndPos = Input.mousePosition;
 
-            if(m_Crosshair.position.x > Mathf.Max(m_TopRight.x,m_BottomLeft.x) ){
-                // Left out of bound
-                m_Crosshair.position = new Vector3(Mathf.Max(m_TopRight.x,m_BottomLeft.x),m_Crosshair.position.y,0);
-            }
+            Vector3 offset = m_AimSensitivity * ( m_AimDragEndPos - m_AimDragStartPos) / 15;
+            m_CrossHair.position = m_CrossHairDragStartPos + offset;
 
-
-            if(m_Crosshair.position.y > Mathf.Max(m_TopRight.y,m_BottomLeft.y) ){
-                // Top out of bound
-                m_Crosshair.position = new Vector3(m_Crosshair.position.x,Mathf.Max(m_TopRight.y,m_BottomLeft.y),0);
-            }
-
-            
-            if(m_Crosshair.position.y < Mathf.Min(m_TopRight.y,m_BottomLeft.y) ){
-                // Down out of bound
-                m_Crosshair.position = new Vector3(m_Crosshair.position.x,Mathf.Min(m_TopRight.y,m_BottomLeft.y),0);
-            }
         }
-
-    }
-
-    private void UpdateBaseCameraBorder(){
-        Vector2 bottomLeft = new Vector2(Mathf.Min(m_TopRight.x,m_BottomLeft.x),Mathf.Min(m_TopRight.y,m_BottomLeft.y));
-        Vector2 bottomRight= new Vector2(Mathf.Max(m_TopRight.x,m_BottomLeft.x),Mathf.Min(m_TopRight.y,m_BottomLeft.y));
-        Vector2 topLeft = new Vector2(Mathf.Min(m_TopRight.x,m_BottomLeft.x),Mathf.Max(m_TopRight.y,m_BottomLeft.y));
-        Vector2 topRight= new Vector2(Mathf.Max(m_TopRight.x,m_BottomLeft.x),Mathf.Max(m_TopRight.y,m_BottomLeft.y));
-        m_BaseCameraBorder.points = new Vector2[]{
-            topRight , topLeft , bottomLeft, bottomRight
-        };
     }
 }
---
+++ Assets/DeadZedAimSystem/Script/DeadZedGunController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class DeadZedGunController : MonoBehaviour
+{
+    [SerializeField] private Transform m_Crosshair; // sway camera 
+    [SerializeField][Range(0.1f, 1.5f)] private float m_MouseSensitivity = 0.5f;
+
+    [SerializeField] private Vector3 m_WeaponToMainCameraOffset = new Vector3(5.5f, -2.5f, 0);
+
+    [SerializeField] private Vector2Int m_TopRight;
+    [SerializeField] private Vector2Int m_BottomLeft;
+    [SerializeField] private PolygonCollider2D m_BaseCameraBorder ;
+
+
+    private void Start()
+    {
+        UpdateBaseCameraBorder();
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Mouse X") < 1 && Input.GetAxis("Mouse Y") < 1)
+        {
+            m_Crosshair.position += new Vector3(
+                Input.GetAxis("Mouse X"),
+                Input.GetAxis("Mouse Y"),
+                0) * m_MouseSensitivity;
+
+            // prevent crosshair going out of bound
+            if(m_Crosshair.position.x < Mathf.Min(m_TopRight.x,m_BottomLeft.x) ){
+                // Left out of bound
+                m_Crosshair.position = new Vector3(Mathf.Min(m_TopRight.x,m_BottomLeft.x),m_Crosshair.position.y,0);
+            }
+
+            if(m_Crosshair.position.x > Mathf.Max(m_TopRight.x,m_BottomLeft.x) ){
+                // Left out of bound
+                m_Crosshair.position = new Vector3(Mathf.Max(m_TopRight.x,m_BottomLeft.x),m_Crosshair.position.y,0);
+            }
+
+
+            if(m_Crosshair.position.y > Mathf.Max(m_TopRight.y,m_BottomLeft.y) ){
+                // Top out of bound
+                m_Crosshair.position = new Vector3(m_Crosshair.position.x,Mathf.Max(m_TopRight.y,m_BottomLeft.y),0);
+            }
+
+            
+            if(m_Crosshair.position.y < Mathf.Min(m_TopRight.y,m_BottomLeft.y) ){
+                // Down out of bound
+                m_Crosshair.position = new Vector3(m_Crosshair.position.x,Mathf.Min(m_TopRight.y,m_BottomLeft.y),0);
+            }
+        }
+
+    }
+
+    private void UpdateBaseCameraBorder(){
+        Vector2 bottomLeft = new Vector2(Mathf.Min(m_TopRight.x,m_BottomLeft.x),Mathf.Min(m_TopRight.y,m_BottomLeft.y));
+        Vector2 bottomRight= new Vector2(Mathf.Max(m_TopRight.x,m_BottomLeft.x),Mathf.Min(m_TopRight.y,m_BottomLeft.y));
+        Vector2 topLeft = new Vector2(Mathf.Min(m_TopRight.x,m_BottomLeft.x),Mathf.Max(m_TopRight.y,m_BottomLeft.y));
+        Vector2 topRight= new Vector2(Mathf.Max(m_TopRight.x,m_BottomLeft.x),Mathf.Max(m_TopRight.y,m_BottomLeft.y));
+        m_BaseCameraBorder.points = new Vector2[]{
+            topRight , topLeft , bottomLeft, bottomRight
+        };
+    }
+}

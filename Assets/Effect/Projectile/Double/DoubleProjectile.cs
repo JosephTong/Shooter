42b95020a649d202319a1b918c2a409dce5f3fe3
JosephTong <joseph.tong@titansworld.co>
2023-06-08 11:51:31 +0800

    Projectile Effect

---
+++ Assets/Effect/Projectile/Double/DoubleProjectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleProjectile : MonoBehaviour
+{
+    [SerializeField] private Transform m_Red;
+    [SerializeField] private Transform m_Blue;
+    [SerializeField][Range(0.1f, 2f)] private float m_Distance = 0.25f;
+    [SerializeField][Range(0.1f, 20f)] private float m_Speed = 4f;
+    private float m_TimePass = 0;
+
+
+    private void Update()
+    {
+        m_TimePass += Time.deltaTime;
+        m_Red.localPosition = new Vector3( m_Distance * Mathf.Sin(m_Speed *m_TimePass), 0, 0);
+        m_Blue.localPosition = new Vector3(-1 * m_Distance * Mathf.Sin(m_Speed *m_TimePass), 0, 0);
+    }
+
+}
